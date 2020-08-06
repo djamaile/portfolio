21e971bb3b7c2ae63b6611ab999f86d56247f0cd
djamaile <djamaile@users.noreply.github.com>
2020-08-06 23:06:06 +0000

    deploy: 766cb932db683307b03dbf20840d92f1733f2222

---
+++ precache-manifest.f1b327a7627a634963eec2e30fe5f3c3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "990ba279b6c0a9bf7b8e9f4528a60814",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e2d356868866537ec034",
+    "url": "/portfolio/static/css/main.d9e75851.chunk.css"
+  },
+  {
+    "revision": "e2c4bf307e8379e9eefd",
+    "url": "/portfolio/static/js/2.6741a6cd.chunk.js"
+  },
+  {
+    "revision": "554d9949e075f5ed61ee4ef6dd8198d2",
+    "url": "/portfolio/static/js/2.6741a6cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c965ee3e6a2001bc83d7",
+    "url": "/portfolio/static/js/3.fa9e67d2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/3.fa9e67d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1395f7d827fe1a800c0",
+    "url": "/portfolio/static/js/4.12431729.chunk.js"
+  },
+  {
+    "revision": "e2d356868866537ec034",
+    "url": "/portfolio/static/js/main.b17ae8c2.chunk.js"
+  },
+  {
+    "revision": "ea9c2ce8b28863f6e2fd",
+    "url": "/portfolio/static/js/runtime-main.c619e253.js"
+  },
+  {
+    "revision": "8762bd10d73b6625796af7a6d57fdaca",
+    "url": "/portfolio/static/media/anatoken.8762bd10.png"
+  },
+  {
+    "revision": "8403b7405baffea67d6744852840de9d",
+    "url": "/portfolio/static/media/glr.8403b740.png"
+  },
+  {
+    "revision": "d268263fbdd601cdf59c1bb756d1f426",
+    "url": "/portfolio/static/media/kanji.d268263f.png"
+  },
+  {
+    "revision": "03fb83e46a378bd794b6896b49d713f7",
+    "url": "/portfolio/static/media/portfolio.03fb83e4.png"
+  }
+]);
