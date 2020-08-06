e374cceca2c680fe59742b729b0145dce84a3452
djamaile <rdjamaile@gmail.com>
2020-08-07 00:58:26 +0200

    removed unused component

--- src/components/home/index.ts
+++ src/components/home/index.ts
@@ -1,4 +1,3 @@
-export { default as App } from "./App";
 export { default as Name } from "./Name";
 export { default as Introduction } from "./Introduction";
 export { default as Socials } from "./Socials";
