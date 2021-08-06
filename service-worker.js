3781af555db6dfb390ddc0aae7cb3019dc020c9c
djamaile <djamaile@users.noreply.github.com>
2021-08-06 01:44:41 +0000

    deploy: fb02ce2ae03db6f2a8923cbef250a59c91401c24

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.00ef30c29d703566c6edeebdfed7d704.js"
+  "/portfolio/precache-manifest.82e7004608c3d0f463071dac220dfb7c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
